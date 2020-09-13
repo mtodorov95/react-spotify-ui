32185b871fca916facf9cabfa1a1c2bbad99b4e7
mtodorov95 <mtodorov95@gmail.com>
2020-09-13 09:42:59 +0300

    header ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { Avatar } from "@material-ui/core";
 
-function Header() {
-  return <div></div>;
+function Header({ spotify }) {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <SearchIcon />
+        <input
+          type="text"
+          placeholder="Search for artists, songs and podcasts"
+        />
+      </div>
+      <div className="header__right">
+        <Avatar src="" alt="user avatar" />
+        <h4>Username</h4>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
